9d120758a40bfce04a68d73bb83fad0f3634262c
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-14 15:54:57 +0200

    template.php
    Ajout d'une page template pour pouvoir gérer la vue

---
+++ template.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="style.css" />
+<title><?= $titre?></title>
+<!--  Elément spécifique -->
+</head>
+<body>
+	<div id="global">
+		<header>
+			<a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
+		</header>
+		<div id="contenu">
+		<?= $contenu ?> <!-- Element spécifique -->
+		</div>
+		<footer id="piedBlog"> Blog réalisé avec PHP, HTML5 et CSS. </footer>
+	</div>
+	<!--  #global -->
+</body>
+</html>
