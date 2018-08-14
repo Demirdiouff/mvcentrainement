32cca699a1e60b61a98a6fa14530afa5f929976d
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-14 15:55:15 +0200

    index.php
    modifié la page index.php pour ne contenir que des include/require et la connexion à la BDD

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 // Accès aux données
-$bdd = new PDO('mysql:host=localhost;dbname=mvcentrainement;charset=utf8', 'root', 'root');
-$billets = $bdd->query('SELECT BIL_ID as id, BIL_DATE as date,' . 'BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' order by BIL_ID desc');
+require 'Modele.php';
+
+$billets = getBillets();
 
 // Affichage
 require 'vueAccueil.php';
