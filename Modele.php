274ce7615c177a20144540c2a4c1e07614fe7a01
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-20 14:45:42 +0200

    ajout de connexion à la bdd et la gest des erreurs

--- Modele.php
+++ Modele.php
@@ -7,3 +7,11 @@ function getBillets() {
     $billets = $bdd->query('SELECT BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' order by BIL_ID desc');
     return $billets;
 }
+
+// Effectue la connexion à la BDD
+// Instancie et renvoi l'objet PDO associé
+
+function getBdd() {
+    $bdd = new PDO('mysql:host=localhost;dbname=mvcentrainement;charset=utf8', 'root', 'root', array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    return $bdd;
+}
