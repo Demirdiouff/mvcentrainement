f8fd64976b11424be21b8a4a4f54e72d2f6c2b4d
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-20 14:44:56 +0200

    correction du require

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 // Accès aux données
 require 'Modele.php';
 
+try {
+    $billets = getBillets();
+    require 'vueAccueil.php';
+} 
+catch (Exception $e) {
+    echo '<html><body>Erreur ! ' . $e->getMesasge() . '</body></html>';
+}
+
 $billets = getBillets();
 
 // Affichage
 require 'vueAccueil.php';
+require 'template.php';
