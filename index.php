c03f7f54252ba2a4df0b9a35085f9bb86b8417e1
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-21 10:50:02 +0200

    modif de l'écriture des recup function

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 require 'Modele.php';
 
 try {
-    $billets = getBillets();
+    $billet = getBillet();
     require 'vueAccueil.php';
 } 
 catch (Exception $e) {
     echo '<html><body>Erreur ! ' . $e->getMesasge() . '</body></html>';
 }
 
-$billets = getBillets();
+$billet = getBillet();
 
 // Affichage
 require 'vueAccueil.php';
