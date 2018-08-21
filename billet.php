0c384bdc8710b94e5cfc0a5daacb842751495084
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-21 12:59:31 +0200

    correction parenthèse - crochet

--- billet.php
+++ billet.php
@@ -5,7 +5,7 @@ require 'Modele.php';
 try {
     if (isset($_GET['id'])) {
         // intval renvoi la valeur numérique du paramètre ou 0 en cas d'échec
-        $id = intval($_GET('id'));
+        $id = intval($_GET['id']);
         if ($id != 0) {
             $billet = getBillet($id);
             $commentaires = getCommentaires($id);
