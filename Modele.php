b4a44fc55254911730eb321842fc96c9eaaecc7b
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-21 10:40:49 +0200

    modif requête getBillets()

--- Modele.php
+++ Modele.php
@@ -4,9 +4,13 @@
 
 function getBillets() {
     $bdd = new PDO('mysql:host=localhost;dbname=mvcentrainement;charset=utf8', 'root', 'root');
-    $billets = $bdd->query('SELECT BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' order by BIL_ID desc');
+    $billets = $bdd->prepare('SELECT BIL_ID as id, BIL_DATE as date,' . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET' . ' where BIL_ID=?');
     return $billets;
 }
+
+// Renvoi la liste des commentaires associés à un billet
+
+
 
 // Effectue la connexion à la BDD
 // Instancie et renvoi l'objet PDO associé
