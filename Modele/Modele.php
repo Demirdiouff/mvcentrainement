fae75d4769eda01f8cc8fc1498c1e3d8b877799f
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-27 03:37:02 +0200

    suppression partie commentaire

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -18,17 +18,6 @@ abstract class Modele
         }
         return $resultat;
     }
-    
-    // Renvoi la liste des commentaires associés à un billet
-    public function getCommentaires($idBillet)
-    {
-        $bdd = getBdd();
-        $commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,' . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE' . ' where BIL_ID=?');
-        $commentaires->execute(array(
-            $idBillet
-        ));
-        return $commentaires;
-    }
 
     // Effectue la connexion à la BDD
     // Instancie et renvoi l'objet PDO associé
