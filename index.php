c0bc8e57e7cda95b2ea73af4a303d1f4a8d3fa59
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-27 14:12:12 +0200

    Modification index.php pour simplifier au maximum en incluant un Routeur.php dans le Controleur

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
 
-//------------- Routeur (point d'arrivée du site) ----------------//
+//------------- Routeur (Contrôleur frontal) (point d'arrivée du site) ----------------//
 
 
-require 'Controleur/Controleur.php';
+require 'Controleur/Routeur.php';
 
-try {
-    if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'billet') {
-            if (isset($_GET['id'])) {
-                $idBillet = intval($_GET['id']);
-                if ($idBillet != 0)
-                    billet($idBillet);
-                else
-                    throw new Exception("Identifiant de billet non valide");
-            }
-            else
-                throw new Exception("Identifiant de billet non défini");
-        }
-        else throw new Exception("Action non valide");
-    }
-    else {
-        accueil(); // action par défaut
-    }
-}
-catch (Exception $e) {
-    erreur($e->getMessage());
-}
+$routeur = new Routeur();
+$routeur->routerRequete();
