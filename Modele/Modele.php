95f72f3691cc6b95db9d4c38e50f9624751b2dd8
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-08-27 03:51:38 +0200

    correction code partie Modele

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -9,23 +9,27 @@ abstract class Modele
     private $bdd;
     
     // Exécute une requête SQL éventuellement paramétrée 
-    protected function executerRequete ($sql, $params = null) {
+    protected function executerRequete($sql, $params = null) {
         if ($params == null) {
             $resultat = $this->getBdd()->query($sql);     // exécution directe
         }
         else {
-            $resultat = $this->getBdd()->prepare($sql);
+            $resultat = $this->getBdd()->prepare($sql);    // requête préparée
+            $resultat->execute($params);
         }
         return $resultat;
     }
 
-    // Effectue la connexion à la BDD
+    // Renvoi un objet de connexion à la BDD en initialisant la connexion au besoin
     // Instancie et renvoi l'objet PDO associé
-    private function getBdd()
-    {
-        $bdd = new PDO('mysql:host=localhost;dbname=mvcentrainement;charset=utf8', 'root', 'root', array(
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ));
-        return $bdd;
+    
+    private function getBdd() {
+        if ($this->bdd == null) {
+            // Création de la connexion 
+            $this->bdd = new PDO('mysql:host=localhost;dbname=mvcentrainement;charset=utf8', 'root', 'root', array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ));
+        }
+        return $this->bdd;
     }
 }
