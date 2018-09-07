1e42bc5e03ad2802593a8159e93299287ccfeb06
Demirdiouff <ozgn-deger_demirdes@hotmail.fr>
2018-09-07 19:31:59 +0200

    ajout du formulaire de commentaire dans la vue a l'intérieur des Romans

--- Vue/vueCommentRoman.php
+++ Vue/vueCommentRoman.php
@@ -22,3 +22,11 @@ $titre = "Un billet simple pour l'\Alaska - " . $billet['titre']; ?>
 	<p><?= $commentaire['contenu'] ?></p>
 <?php endforeach; ?>
 
+<form method="post" action="index.php?action=commenter">
+
+	<input id="auteur" name="auteur" type="text" placeholder="Votre pseudo" required /></br>
+	<textarea id="txtCommentaire" name="contenu" rows="4" placeholder="Votre commentaire" required></textarea></br>
+	<input type="hidden" name="id" value="<?= $billet['id'] ?>" />
+	<input id="validerComment" type="submit" value="Commenter" />
+</form>
+
